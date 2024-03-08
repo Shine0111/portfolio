69527eea2bdc892ca070fdf058c51270a171c1c2
Shine0111 <ranshine9@gmail.com>
2024-03-08 14:04:39 +0100

    added about me section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,18 @@
+import styles from "./About.module.css";
+import classNames from "classnames";
+
 const About = () => {
-  return <div>About Me</div>;
+  return (
+    <div className={classNames("container-max-width", styles.container)}>
+      About Me <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique aut
+        corrupti dolorum hic ipsam porro, qui voluptates, nesciunt ut pariatur,
+        tenetur neque eveniet. Aut praesentium consequatur accusamus magnam
+        blanditiis excepturi!
+      </p>
+    </div>
+  );
 };
 
 export default About;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,33 +3,38 @@ import styles from "./HomePage.module.css";
 import useViewport from "../../hooks/useViewport";
 import GlitchText from "../../components/GlicthText/GlitchText";
 import heroImage from "../../assets/portfolio-hero-v1.svg";
+import About from "../../components/About/About";
 
 function HomePage() {
   const { viewWidth } = useViewport();
   const roles = ["front-end developer", "javascript developer"];
   return (
-    <div className={classNames("hero-container-max-width", styles.container)}>
-      <div className={styles.heroTextContainer}>
-        <h1 className={styles.heroTitle}>
-          Hi, my <br /> name is <span className={styles.name}>Shine</span>
-          <span className={styles.styledPoint}>.</span>
-        </h1>
-        <h2 className={styles.description}>
-          I'm a
-          <span className={styles.role}>
-            <GlitchText texts={roles} />
-          </span>
-          <span className={styles.styledPoint}>.</span> I build awesome websites
-          {""} <br />
-          and bring creative ideas to life on the web and on mobiles.
-        </h2>
-      </div>
-      {viewWidth > 1120 && (
-        <div className={styles.svgContainer}>
-          <img src={heroImage} alt="hero" />
+    <>
+      <div className={classNames("hero-container-max-width", styles.container)}>
+        <div className={styles.heroTextContainer}>
+          <h1 className={styles.heroTitle}>
+            Hi, my <br /> name is <span className={styles.name}>Shine</span>
+            <span className={styles.styledPoint}>.</span>
+          </h1>
+          <h2 className={styles.description}>
+            I'm a
+            <span className={styles.role}>
+              <GlitchText texts={roles} />
+            </span>
+            <span className={styles.styledPoint}>.</span> I build awesome
+            websites
+            {""} <br />
+            and bring creative ideas to life on the web and on mobiles.
+          </h2>
         </div>
-      )}
-    </div>
+        {viewWidth > 1168 && (
+          <div className={styles.svgContainer}>
+            <img src={heroImage} alt="hero" />
+          </div>
+        )}
+      </div>
+      <About />
+    </>
   );
 }
 
