21c5bd34620718cca128f52429866337e00112c0
Shine0111 <ranshine9@gmail.com>
2024-03-11 10:41:15 +0100

    added bookshelf and projects placeholder

--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -1,10 +1,28 @@
 import classNames from "classnames";
 import styles from "./ProjectsSection.module.css";
+import placeholderImage from "../../assets/placeholder-img.jpg";
 
 const ProjectsSection = () => {
+  const projects = [
+    { title: "project1", image: placeholderImage },
+    { title: "project2", image: placeholderImage },
+    { title: "project3", image: placeholderImage },
+  ];
   return (
     <div className={classNames("container-max-width-wider", styles.container)}>
-      Projects
+      <img
+        src={placeholderImage}
+        alt="placeholder image"
+        className={styles.projectEntryImage}
+      />
+      {projects.map((project) => (
+        <img
+          key={project.title}
+          src={project.image}
+          alt={project.title}
+          className={styles.projectEntryImage}
+        />
+      ))}
     </div>
   );
 };
