68dc211ca8937bdae4a88ae5d414e65a827704a2
Shine0111 <ranshine9@gmail.com>
2024-03-13 12:31:09 +0100

    responsive projects

--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -1,16 +1,26 @@
 import classNames from "classnames";
 import styles from "./ProjectsSection.module.css";
 import placeholderImage from "../../assets/placeholder-img.jpg";
+import useViewport from "../../hooks/useViewport";
 
 const ProjectsSection = () => {
+  const { viewWidth, breakpoint } = useViewport();
+
+  const getProjects = () => {
+    if (viewWidth < breakpoint) return [projects[0], projects[1]];
+    return projects;
+  };
+
   const projects = [
     { title: "project1", image: placeholderImage },
     { title: "project2", image: placeholderImage },
     { title: "project3", image: placeholderImage },
   ];
+
+  const projectsLoaded: { title: string; image: string }[] = getProjects();
   return (
     <div className={classNames("container-max-width-wider", styles.container)}>
-      {projects.map((project) => (
+      {projectsLoaded.map((project) => (
         <img
           key={project.title}
           src={project.image}
