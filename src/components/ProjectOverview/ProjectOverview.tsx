117c92adfd939f2c12558d2438f6543335cfb731
Shine Ran <ranshine9@gmail.com>
2024-07-14 22:00:15 +0300

    repos url

--- src/components/ProjectOverview/ProjectOverview.tsx
+++ src/components/ProjectOverview/ProjectOverview.tsx
@@ -4,6 +4,7 @@ import { Project } from "../../data/data";
 import classNames from "classnames";
 import { MdArrowBackIos } from "react-icons/md";
 import useViewport from "../../hooks/useViewport";
+import github from "../../assets/icons8-github.svg";
 
 interface ProjectOverviewProps {
   project: Project;
@@ -33,20 +34,43 @@ const ProjectOverview = ({ project }: ProjectOverviewProps) => {
       </div>
       <div className={styles.projectOverviewContainer}>
         <div className={styles.imageContainer}>
-          <img src={project?.image} />
+          <img src={project?.image} className={styles.projectCover} />
           {viewWidth > 768 && (
-            <div
-              className={styles.button}
-              onClick={() => window.open(project?.url, "_blank")}
-            >
-              Demo
-            </div>
+            <>
+              <button
+                className={styles.button}
+                onClick={() => window.open(project?.url, "_blank")}
+              >
+                <p>Demo</p>
+              </button>
+              <button
+                className={styles.buttonGithub}
+                onClick={() => window.open(project?.githubUrl, "_blank")}
+              >
+                <img src={github} />
+                <p>Repository</p>
+              </button>
+            </>
           )}
         </div>
         <div className={styles.projectDetailsContainer}>
           <h1>{project?.title}</h1>
-          <p onClick={() => navigate("/")}>{project?.author}</p>
+          <p
+            onClick={() =>
+              window.open("https://github.com/Shine0111", "_blank")
+            }
+          >
+            {project?.author}
+          </p>
           <p>{project?.summary}</p>
+          <div className={styles.technologiesContainer}>
+            <h3>Technologies :</h3>
+            {project?.technologies.map((technology) => (
+              <div className={styles.technologyContainer}>
+                <img src={technology.icon} alt={technology.technology} />
+              </div>
+            ))}
+          </div>
         </div>
         <div>
           {viewWidth <= 768 && (
--- src/data/data.ts
+++ src/data/data.ts
@@ -1,6 +1,11 @@
 import cover1 from "../assets/cover1.webp";
 import cover2 from "../assets/cover2.webp";
 import cover3 from "../assets/cover3.jpg";
+import javascript from "../assets/icons8-javascript.svg";
+import typescript from "../assets/icons8-typescript.svg";
+import html from "../assets/icons8-html.svg";
+import css from "../assets/icons8-css.svg";
+import react from "../assets/icons8-react.svg";
 
 export interface Project {
   id: number;
@@ -10,38 +15,106 @@ export interface Project {
   author: string;
   summary: string;
   url: string;
+  githubUrl: string;
+  technologies: Technologies[];
+}
+
+interface Technologies {
+  technology: string;
+  icon: string;
 }
 
 const projects: Project[] = [
   {
     id: 1,
-    title: "project1",
+    title: "Pikla Landing Page",
     image: cover1,
     category: "website",
     author: "Shine Randriamialison",
     summary:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique autcorrupti dolorum hic ipsam porro, qui voluptates, nesciunt ut pariatur, tenetur neque eveniet. Aut praesentium consequatur accusamus magnam blanditiis excepturi! lsdjkf sldkfj lsdkfj lskdjf lskjdflksj dflks dflkjsldkfjlksd fjlks jdflks jdlfk slkdfjlksjd flks jdflk jslkdfjlskdjflksdj flks dlfkjslkdf jlksdjflks jdlkf lskdf jlskdf jlks dflks jdflk jslkdfjlksd jflk sdflk sdlkf jlskd fjlksd jflks djflk sjdlfk jslkdf jlksd jflksd fjlksdj flks jdflks jdflkjsdflk slkdfjlks fls Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique autcorrupti dolorum hic ipsam porro, qui voluptates, nesciunt ut pariatur, tenetur neque eveniet. Aut praesentium consequatur accusamus magnam blanditiis excepturi! lsdjkf sldkfj lsdkfj lskdjf lskjdflksj dflks dflkjsldkfjlksd fjlks jdflks jdlfk slkdfjlksjd flks jdflk jslkdfjlskdjflksdj flks dlfkjslkdf jlksdjflks jdlkf lskdf jlskdf jlks dflks jdflk jslkdfjlksd jflk sdflk sdlkf jlskd fjlksd jflks djflk sjdlfk jslkdf jlksd jflksd fjlksdj flks jdflks jdflkjsdflk slkdfjlks fls Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique autcorrupti dolorum hic ipsam porro, qui voluptates, nesciunt ut pariatur, tenetur neque eveniet. Aut praesentium consequatur accusamus magnam blanditiis excepturi! lsdjkf sldkfj lsdkfj lskdjf lskjdflksj dflks dflkjsldkfjlksd fjlks jdflks jdlfk slkdfjlksjd flks jdflk jslkdfjlskdjflksdj flks dlfkjslkdf jlksdjflks jdlkf lskdf jlskdf jlks dflks jdflk jslkdfjlksd jflk sdflk sdlkf jlskd fjlksd jflks djflk sjdlfk jslkdf jlksd jflksd fjlksdj flks jdflks jdflkjsdflk slkdfjlks fls Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique autcorrupti dolorum hic ipsam porro, qui voluptates, nesciunt ut pariatur, tenetur neque eveniet. Aut praesentium consequatur accusamus magnam blanditiis excepturi! lsdjkf sldkfj lsdkfj lskdjf lskjdflksj dflks dflkjsldkfjlksd fjlks jdflks jdlfk slkdfjlksjd flks jdflk jslkdfjlskdjflksdj flks dlfkjslkdf jlksdjflks jdlkf lskdf jlskdf jlks dflks jdflk jslkdfjlksd jflk sdflk sdlkf jlskd fjlksd jflks djflk sjdlfk jslkdf jlksd jflksd fjlksdj flks jdflks jdflkjsdflk slkdfjlks fls Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique autcorrupti dolorum hic ipsam porro, qui voluptates, nesciunt ut pariatur, tenetur neque eveniet. Aut praesentium consequatur accusamus magnam blanditiis excepturi! lsdjkf sldkfj lsdkfj lskdjf lskjdflksj dflks dflkjsldkfjlksd fjlks jdflks jdlfk slkdfjlksjd flks jdflk jslkdfjlskdjflksdj flks dlfkjslkdf jlksdjflks jdlkf lskdf jlskdf jlks dflks jdflk jslkdfjlksd jflk sdflk sdlkf jlskd fjlksd jflks djflk sjdlfk jslkdf jlksd jflksd fjlksdj flks jdflks jdflkjsdflk slkdfjlks fls",
     url: "https://pikla-landing-page.vercel.app/",
+    githubUrl: "https://github.com/Shine0111/pikla-landing",
+    technologies: [
+      {
+        technology: "React",
+        icon: react,
+      },
+      {
+        technology: "Javascript",
+        icon: javascript,
+      },
+      {
+        technology: "Typescript",
+        icon: typescript,
+      },
+      {
+        technology: "CSS",
+        icon: css,
+      },
+      {
+        technology: "HTML",
+        icon: html,
+      },
+    ],
   },
   {
     id: 2,
-    title: "project2",
+    title: "GameHub",
     image: cover2,
     category: "website",
     author: "Shine Randriamialison",
     summary:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique autcorrupti dolorum hic ipsam porro, qui voluptates, nesciunt ut pariatur, tenetur neque eveniet. Aut praesentium consequatur accusamus magnam blanditiis excepturi!",
     url: "https://game-hub-pink-ten.vercel.app/",
+    githubUrl: "https://github.com/Shine0111/game-hub",
+    technologies: [
+      {
+        technology: "Javascript",
+        icon: javascript,
+      },
+      {
+        technology: "Typescript",
+        icon: typescript,
+      },
+      {
+        technology: "React",
+        icon: react,
+      },
+      {
+        technology: "HTML",
+        icon: html,
+      },
+      {
+        technology: "CSS",
+        icon: css,
+      },
+    ],
   },
   {
     id: 3,
-    title: "project3",
+    title: "Tamba Be",
     image: cover3,
     category: "website",
     author: "Shine Randriamialison",
     summary:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique autcorrupti dolorum hic ipsam porro, qui voluptates, nesciunt ut pariatur, tenetur neque eveniet. Aut praesentium consequatur accusamus magnam blanditiis excepturi!",
     url: "https://shine0111.github.io/tambaBe/",
+    githubUrl: "https://github.com/Shine0111/tambaBe",
+    technologies: [
+      {
+        technology: "Javascript",
+        icon: javascript,
+      },
+      {
+        technology: "HTML",
+        icon: html,
+      },
+      {
+        technology: "CSS",
+        icon: css,
+      },
+    ],
   },
 ];
 
