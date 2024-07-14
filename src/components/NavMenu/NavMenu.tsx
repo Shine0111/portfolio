09a9f9b294f234a2bc28109a8fc2a8052ead87d9
Shine Ran <ranshine9@gmail.com>
2024-07-14 16:48:39 +0300

    responsive mobile menu position

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -1,17 +1,30 @@
+import classNames from "classnames";
 import styles from "./NavMenu.module.css";
+import { useLocation } from "react-router-dom";
+import useViewport from "../../hooks/useViewport";
 
 interface Props {
   mobile?: boolean;
 }
 
 const NavMenu = ({ mobile }: Props) => {
+  const location = useLocation();
+  const { viewWidth } = useViewport();
+
+  const getClassName = () => {
+    return classNames({
+      [styles.menuUrlsContainer]: !mobile,
+      [styles.menuUrlsContainerMobile]: mobile && location.pathname === "/",
+      [styles.menuUrlsContainerMobileLower]:
+        mobile && location.pathname !== "/",
+      [styles.menuUrlsContainerMobileLowerLower]:
+        mobile && location.pathname !== "/" && viewWidth < 768,
+    });
+  };
+
   return (
     <div>
-      <ul
-        className={
-          mobile ? styles.menuUrlsContainerMobile : styles.menuUrlsContainer
-        }
-      >
+      <ul className={getClassName()}>
         <a href="">
           <li>HOME</li>
         </a>
