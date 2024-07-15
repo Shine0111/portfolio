91fc134756d8a38eee9b576ad791f1f368e435d8
Shine Ran <ranshine9@gmail.com>
2024-07-15 13:09:10 +0300

    emailjs implementation

---
+++ public/env.js
@@ -0,0 +1,5 @@
+window.env = {
+  "EMAIL_SERVICE_ID": "service_97ir4be",
+  "EMAIL_TEMPLATE_ID": "template_9aefz3k",
+  "EMAIL_PUBLIC_KEY": "vEe8sefOzDLFP-3PI"
+};
--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,14 +1,93 @@
+import { useState, ChangeEvent, FormEvent } from "react";
+import emailjs from "@emailjs/browser";
 import classNames from "classnames";
 import styles from "./Contact.module.css";
 
-const Contact = () => {
+interface FormData {
+  name: string;
+  email: string;
+  message: string;
+}
+
+const Contact: React.FC = () => {
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const templateParams: Record<string, string> = {
+      name: formData.name,
+      email: formData.email,
+      message: formData.message,
+    };
+
+    emailjs
+      .send(
+        import.meta.env.VITE_EMAIL_SERVICE_ID,
+        import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+        templateParams,
+        import.meta.env.VITE_EMAIL_PUBLIC_KEY
+      )
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        (err) => {
+          console.log("FAILED...", err);
+        }
+      );
+  };
+
   return (
     <div>
       <div className={styles.titleContainer}>
         <h2 className="italic-title">Get in touch</h2>
       </div>
       <div className={classNames("container-max-width", styles.formContainer)}>
-        Email
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            ></textarea>
+          </div>
+          <button type="submit">Send</button>
+        </form>
       </div>
     </div>
   );
